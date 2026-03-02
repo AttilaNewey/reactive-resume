823639fe06fdfffa65cbc23abdf1a9105b583837
AttilaNewey <anewey@uchicago.edu>
2026-03-01 22:35:44 -0600

    saving grace

---
+++ js/file.js
@@ -0,0 +1,21 @@
+function hideThis (ele) {
+    ele.css({
+      display: 'none'
+    })
+  }
+
+  function tabClick () {
+    // hide all the content elements first
+    nn.getAll('.skillscontent div').forEach(hideThis)
+    // then show the one with matching data-cnt
+    nn.get(this.data.cnt).css({
+      display: 'flex',border: '2px solid',padding: '20px', marginLeft: '1%', marginRight: '1%', marginTop: '-2px', zIndex: '12',
+    })
+  }
+  
+  function applyClicker (ele) {
+    ele.on('mouseover', tabClick)
+  }
+
+  
+  nn.getAll('.tab').forEach(applyClicker)
