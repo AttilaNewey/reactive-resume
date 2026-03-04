d50ebb8a2c3cdad17630ab10b2174e0050fc0db6
AttilaNewey <anewey@uchicago.edu>
2026-03-03 20:16:31 -0600

    final test

--- js/file.js
+++ js/file.js
@@ -9,8 +9,15 @@ function hideThis (ele) {
     nn.getAll('.skillscontent div').forEach(hideThis)
     // then show the one with matching data-cnt
     nn.get(this.data.cnt).css({
-      display: 'flex', border: '2px solid',padding: '20px', marginLeft: '1%', marginRight: '1%', marginTop: '-2px', zIndex: '12', borderBottomLeftRadius: '25px', borderBottomRightRadius: '25px',
-      borderTopRightRadius: '25px',
+      display: 'flex', 
+      justifyContent: 'spaceBetween',
+      boxShadow: 'rgba(0, 0, 0, 0.24) 0px 3px 8px',
+      padding: '2%', 
+      marginLeft: '1%', 
+      marginRight: '1%', 
+      marginTop: '-2px', 
+      zIndex: '12', 
+      borderRadius: '25px',
     })
   }
   
